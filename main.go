60a56ca5630b92e606489a128fcf7c3a8a259b31
Bartek <bmroczkowski@icloud.com>
2024-04-14 21:25:50 +0200

    Add option menu

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func hash(passw string) string {
@@ -18,11 +19,46 @@ func hash(passw string) string {
 	return hexHash
 }
 
+func menu(option string) {
+	if option != "gen" && option != "val" && option != "hash" {
+		fmt.Println("Invalid option")
+		return
+	}
+
+	if option == "gen" {
+		fmt.Println("Generate password")
+		return
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Type password: ")
+	scanner.Scan()
+	password := scanner.Text()
+
+	if option == "val" {
+		fmt.Println("Validate password")
+	} else if option == "hash" {
+		fmt.Println("Hashed password:", hash(password))
+	}
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("Type a password: ")
-	scanner.Scan()
+	fmt.Print("Choose an option:\n" +
+		"  - gen [generate password]\n" +
+		"  - val [validate password]\n" +
+		"  - hash [hash password]\n" +
+		"  - quit [quit loop]\n")
 
-	input := scanner.Text()
-	fmt.Println("Hashed password:", hash(input))
+	for {
+		fmt.Printf("\n> ")
+		err := scanner.Scan()
+		option := strings.ToLower(scanner.Text())
+
+		if option == "quit" || !err {
+			break
+		}
+
+		menu(option)
+	}
 }
